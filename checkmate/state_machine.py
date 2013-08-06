ca267f7aebd64427bb99e6a0530decbb1cee7892
Vincent Croisier <vcroisier.beijing@gmail.com>
2013-08-06 13:00:45 +0800

    Fix loop in develop state_machine

--- checkmate/state_machine.py
+++ checkmate/state_machine.py
@@ -25,7 +25,6 @@ class StateMachine(object):
         """
         for transition in self.transitions:
             if transition.is_matching_initial(states):
-                local_runs = copy.deepcopy(runs)
                 local_states = copy.deepcopy(states)
 
                 try:
@@ -36,9 +35,8 @@ class StateMachine(object):
                 outgoing_exchange_list = transition.process(local_states, incoming_exchange)
             
                 this_run = checkmate.run.Run(states, incoming_exchange, local_states, outgoing_exchange_list)
-                if this_run not in local_runs:
-                    local_runs.append(this_run)
-                    #runs.append(([s.value for s in states], transition.incoming.code, [s.value for s in local_states]))
-                    runs = self.develop(local_states, local_runs)
+                if this_run not in runs:
+                    runs.append(this_run)
+                    runs = self.develop(local_states, runs)
         return runs
 
