ae51a5f1bfbb441158a3d2d0cd0ee6761484c237
jeff <none@none>
2013-08-08 18:02:41 +0800

    update __eq__ in checkmate.partition to add attribute compare

--- checkmate/partition.py
+++ checkmate/partition.py
@@ -60,9 +60,8 @@ class Partition(object):
 
         for name in dir(self):
             attr = getattr(self, name)
-            if checkmate._storage.IStorage.providedBy(attr):
-                attr = attr.factory()
-                setattr(self, name, attr)
+            attr = attr.factory()
+            setattr(self, name, attr)
 
     def __dir__(self):
         return self.partition_attribute
@@ -74,23 +73,73 @@ class Partition(object):
             >>> Partition('AT1') == Partition('AT1')
             True
         """
-        if self._queue == True:
-            if len(self.value) == 0:
-                return (len(other.value) == 0 or other.value[0] == None)
-            elif len(other.value) == 0:
-                return (len(self.value) == 0 or self.value[0] == None)
-            elif self.value[0] == None or other.value[0] == None:
-                return True
-            else:
-                return (self.value == other.value)
-        if self.value == None or other.value == None:
-            return True
-        else:
-            return (self.value == other.value)
+        if type(self) != type(other):
+            return False
+        return compare_value(self, other) and compare_attr(self, other)
+
 
     def description(self):
         try:
             return (self.partition_storage.get_description(self))
         except AttributeError:
             return (None,None,None)
+
+def compare_value(one, other):
+    """
+    >>> checkmate.partition.compare_value(Partition('AT1'), Partition('AT1'))
+    True
+    >>> checkmate.partition.compare_value(Partition('AT1'), Partition('AT2'))
+    False
+    """
+    if one._queue == True:
+        if len(one.value) == 0:
+            return (len(other.value) == 0 or other.value[0] == None)
+        elif len(other.value) == 0:
+            return (len(one.value) == 0 or one.value[0] == None)
+        elif one.value[0] == None or other.value[0] == None:
+            return True
+        else:
+            return (one.value == other.value)
+    if one.value == None or other.value == None:
+        return True
+    else:
+        return (one.value == other.value)
+
+def compare_attr(one, other):
+    """
+    >>> checkmate.partition.compare_attr(Partition('AT1'), Partition('AT2'))
+    True
+    >>> import checkmate.exchange
+    >>> checkmate.partition.compare_attr(Partition('AT1'), checkmate.exchange.Exchange('R'))
+    False
+    >>> import sample_app.data_structure
+    >>> import sample_app.exchanges 
+    >>> import checkmate.state
+    >>> import collections
+    >>> import zope.interface
+    >>> import checkmate.partition_declarator
+    >>> de = checkmate.partition_declarator.Declarator(sample_app.data_structure, checkmate.state, sample_app.exchanges)
+    >>> par = de.new_partition('data_structure', "TestActionPriority", standard_methods = {}, codes=["P0('NORM')", "P0('HIGH')"], full_description=collections.OrderedDict([("P0('NORM')",('D-PRIO-01', 'NORM valid value', 'NORM priority value')), ("P0('HIGH')",('D-PRIO-02', 'HIGH valid value', 'HIGH priority value'))]))
+    >>> ar = de.new_partition('data_structure', 'TestActionRequest(P=TestActionPriority)', standard_methods = {}, codes=[], full_description=collections.OrderedDict())
+    >>> c = ar[-1].storage[0].factory()
+    >>> c1 = ar[-1].storage[0].factory()
+    >>> c.P.value
+    'NORM'
+    >>> c1 == c
+    True
+    >>> c1.P.value = 'HIGH'
+    >>> c1 == c
+    False
+    """
+    if type(one) != type(other):
+        return False
+    for name in dir(one):
+        attr = getattr(one, name)
+        if not (hasattr(other, name) and attr == getattr(other, name)):
+            return False
+    for name in dir(other):
+        attr = getattr(other, name)
+        if not (hasattr(one, name) and attr == getattr(one, name)):
+            return False
+    return True 
 
