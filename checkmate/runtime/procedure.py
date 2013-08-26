8282cd5faff494c6cc73baeb717686cf91455471
Vincent Croisier <vcroisier.beijing@gmail.com>
2013-08-26 21:04:51 +0800

    First implementation of runtime.Procedure + test case

--- checkmate/runtime/procedure.py
+++ checkmate/runtime/procedure.py
@@ -1,18 +1,52 @@
+import nose.plugins.skip
+
 import zope.interface
 
+import checkmate.component
+import checkmate.runtime.registry
 import checkmate.runtime.interfaces
+
 
 @zope.interface.implementer(checkmate.runtime.interfaces.IProcedure)
 class Procedure(object):
     def __init__(self, test=None):
         self.test = test
         
-    def __call__(self, result=None, system_under_test=[], *args):
+    def __call__(self, result, system_under_test, *args):
+        """"""
+        if not self._components_match_sut(system_under_test):
+            raise nose.plugins.skip.SkipTest("Procedure components do not match SUT")
+
         if result is not None:
             result.startTest(self)  
+
+        self.system_under_test = system_under_test
+        for _node in self.exchanges.nodes:
+            _origin = _node.root.origin
+            if _origin not in self.system_under_test:
+                stub = checkmate.runtime.registry.global_registry.getUtility(checkmate.component.IComponent, _origin)
+                stub.simulate(self.exchanges.root)
+                self._follow_up(self.exchanges)
+                
+        if result is not None:
             result.addSuccess(self)
+        if result is not None:
             result.stopTest(self)
-                    
+
+    def _components_match_sut(self, system_under_test):
+        for _sut in system_under_test:
+            if _sut not in self.components:
+                return False
+        return True
+
+    def _follow_up(self, node):
+        for _next in node.nodes:
+            if _next.root.destination not in self.system_under_test:
+                stub = checkmate.runtime.registry.global_registry.getUtility(checkmate.component.IComponent, _next.root.destination)
+                if not stub.validate(_next.root):
+                    raise Exception('Not received')
+        for _next in node.nodes:
+            self._follow_up(_next)
 
     def shortDescription(self):
         """
--- checkmate/runtime/test_procedure.py
+++ checkmate/runtime/test_procedure.py
@@ -1,5 +1,52 @@
+import checkmate._tree
+import checkmate.test_data
 import checkmate.runtime.procedure
 
 class TestProcedure(checkmate.runtime.procedure.Procedure):
     """"""
+    def __init__(self, test=None):
+        """
+            >>> import checkmate.test_data
+            >>> import checkmate.runtime._runtime
+            >>> import checkmate.runtime.communication
+            >>> a = checkmate.test_data.App()
+            >>> r = checkmate.runtime._runtime.Runtime(a, checkmate.runtime.communication.Communication())
+            >>> r.setup_environment(['C1'])
+            >>> r.start_test()
+            >>> proc = TestProcedure()
+            >>> proc.exchanges.root
+            []
+            >>> proc.exchanges.nodes[0].root.action
+            'AC'
+            >>> proc.exchanges.nodes[0].root.origin
+            'C2'
+            >>> proc.exchanges.nodes[0].root.destination
+            'C1'
+            >>> proc.exchanges.nodes[0].nodes[0].root.action
+            'RE'
+            >>> proc.exchanges.nodes[0].nodes[0].root.origin
+            'C1'
+            >>> proc.exchanges.nodes[0].nodes[0].root.destination
+            'C3'
+            >>> proc(result=None, system_under_test=['C1'])
+            Traceback (most recent call last):
+            ...
+            Exception: Not received
+        """
+        super(TestProcedure, self).__init__(test)
+        a = checkmate.test_data.App()
+        c1 = a.components['C1']
+        c2 = a.components['C2']
+        c3 = a.components['C3']
+        a.start()
+        transition = c2.state_machine.transitions[0]
+        self.exchanges = checkmate._tree.Tree(transition.generic_incoming(c2.states), [])
+        for _e in transition.process(c2.states, self.exchanges.root):
+            _e.origin_destination(c2.name, c1.name)
+            self.exchanges.add_node(checkmate._tree.Tree(_e, []))
+        transition = c1.state_machine.transitions[0]
+        for _e in transition.process(c1.states, self.exchanges.nodes[0].root):
+            _e.origin_destination(c1.name, c3.name)
+            self.exchanges.nodes[0].add_node(checkmate._tree.Tree(_e, []))
+        self.components = ('C1', 'C2', 'C3')
 
