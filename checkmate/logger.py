452dac416d88f0954d160c9b0072598c0c22937a
Vincent Croisier <vcroisier.beijing@gmail.com>
2013-09-17 12:29:15 +0800

    Fix typo

--- checkmate/logger.py
+++ checkmate/logger.py
@@ -3,12 +3,14 @@ import logging
 
 logger = logging.getLogger("checkmate")  
 logger.setLevel(logging.DEBUG)  
-# and a general file log handler to handle DEBUG level log message
+
+# add a general file log handler to handle DEBUG level log message
 fhandler=logging.FileHandler("runtime.log")
 fhandler.setLevel(logging.DEBUG)
 formatter = logging.Formatter("[%(asctime)s] - [%(name)s] - %(levelname)s: %(message)s")
 fhandler.setFormatter(formatter)
 logger.addHandler(fhandler)
+
 #add a streamHandler to handler the ERROR level log message
 shandler=logging.StreamHandler()
 shandler.setLevel(logging.ERROR)
