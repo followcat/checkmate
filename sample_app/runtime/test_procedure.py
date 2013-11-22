e370083fc350d7dc0c06311ad105206489b22e83
Vincent Croisier <vcroisier.beijing@gmail.com>
2013-11-22 09:15:09 +0800

    Add initial attribute to procedure generator in sample_app/runtime/test_procedure.py

--- sample_app/runtime/test_procedure.py
+++ sample_app/runtime/test_procedure.py
@@ -34,6 +34,9 @@ class TestProcedureThreaded(checkmate.runtime.procedure.Procedure):
         c3 = a.components['C3']
         a.start()
         transition = c2.state_machine.transitions[0]
+        a.get_initial_transitions()
+        self.itp_transitions = a.initial_transitions
+        self.initial = [_s.storage[0] for (_i, _s) in c1.state_machine.states]
         self.exchanges = checkmate._tree.Tree(c2.process(transition.generic_incoming(c2.states))[0], [])
         for _e in c1.process([self.exchanges.root]):
             self.exchanges.add_node(checkmate._tree.Tree(_e, []))
@@ -70,27 +73,33 @@ class TestProcedureThreaded2(checkmate.runtime.procedure.Procedure):
         c3 = a.components['C3']
         a.start()
         transition = c2.state_machine.transitions[0]
-        self.exchanges = checkmate._tree.Tree(c2.simulate(transition.outgoing[0].factory())[0], [])
-        for _e in c1.process([self.exchanges.root]):
-            self.exchanges.add_node(checkmate._tree.Tree(_e, []))
-        for _e in c3.process([self.exchanges.nodes[0].root]):
-            self.exchanges.nodes[0].add_node(checkmate._tree.Tree(_e, []))
-        for _e in c2.process([self.exchanges.nodes[1].root]):
-            self.exchanges.nodes[1].add_node(checkmate._tree.Tree(_e, []))
-        c1.process([self.exchanges.nodes[1].nodes[0].root])
+        local_exchanges = checkmate._tree.Tree(c2.simulate(transition.outgoing[0].factory())[0], [])
+        for _e in c1.process([local_exchanges.root]):
+            local_exchanges.add_node(checkmate._tree.Tree(_e, []))
+        for _e in c3.process([local_exchanges.nodes[0].root]):
+            local_exchanges.nodes[0].add_node(checkmate._tree.Tree(_e, []))
+        for _e in c2.process([local_exchanges.nodes[1].root]):
+            local_exchanges.nodes[1].add_node(checkmate._tree.Tree(_e, []))
+        c1.process([local_exchanges.nodes[1].nodes[0].root])
 
         transition = c2.state_machine.transitions[2]
         self.exchanges = checkmate._tree.Tree(c2.simulate(transition.outgoing[0].factory())[0], [])
+        a.get_initial_transitions()
+        self.itp_transitions = a.initial_transitions
+        self.initial = c3.get_transition_by_input([self.exchanges.root]).initial
         for _e in c3.process([self.exchanges.root]):
             self.exchanges.add_node(checkmate._tree.Tree(_e, []))
+        #self.initial.extend(c1.get_transition_by_input([self.exchanges.nodes[0].root]).initial)
         for _e in c1.process([self.exchanges.nodes[0].root]):
             self.exchanges.nodes[0].add_node(checkmate._tree.Tree(_e, []))
 
-def build_procedure(exchanges, output):
+def build_procedure(exchanges, output, initial, initial_transitions):
     class TestProc(checkmate.runtime.procedure.Procedure):
         """"""
             
     proc = TestProc()
+    setattr(proc, 'initial', initial)
+    setattr(proc, 'itp_transitions', initial_transitions)
     setattr(proc, 'exchanges', checkmate._tree.Tree(exchanges[0], [checkmate._tree.Tree(_o, []) for _o in output]))
     return proc
 
@@ -100,11 +109,13 @@ def TestProcedureGenerator(application_class=checkmate.test_data.App):
     c2 = a.components['C2']
     c3 = a.components['C3']
     a.start()
+    a.get_initial_transitions()
     #Skip the last two transitions as no outgoing sent to 'C1'
     #Skip the third transition from the last as 'C3' state does not match
     for _t in range(len(c2.state_machine.transitions)-3):
         transition = c2.state_machine.transitions[_t]
         _i = c2.process(transition.generic_incoming(c2.states))
+        initial = c1.get_transition_by_input(_i).initial
         _o = c1.process(_i)
-        yield build_procedure(_i, _o), c2.name, _i[0].action
+        yield build_procedure(_i, _o, initial, a.initial_transitions), c2.name, _i[0].action
 
