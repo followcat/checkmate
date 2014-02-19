34520b65f32c53e20165faeda9f9f0eebea407e6
followcat <followcat@163.com>
2014-02-19 17:39:12 +0800

    Simplify the use of get_itp_from_feature():use fresher.cuke and fresher.cuke.FresherHandler class remove the unused import os
    --HG--
    branch : freshen

--- checkmate/parser/feature_visitor.py
+++ checkmate/parser/feature_visitor.py
@@ -3,13 +3,12 @@
 # Experimental - a non-nose runner for tests, may end up being compatible
 # with Cucumber commandline
 
-import os
 import sys
 import logging
 
+import fresher.cuke
 import fresher.core
 import fresher.context
-import fresher.handlers
 import fresher.stepregistry
 
 import checkmate._utils
@@ -17,91 +16,47 @@ import checkmate._storage
 import checkmate.partition_declarator
 
 
-class FreshenHandlerProxy(object):
-    """ Acts as a handler and proxies callback events to a list of actual handlers. """
-        
-    def __init__(self, handlers):
-        self._handlers = handlers
-    
-    def __getattr__(self, attr):
-        def proxy(*args, **kwargs):
-            for h in self._handlers:
-                method = getattr(h, attr)
-                method(*args, **kwargs)
-        return proxy
-
-
-def run_scenario(step_registry, scenario, handler):
-    
-    runner = fresher.core.StepsRunner(step_registry)
-    fresher.context.scc.clear()
-    
-    for hook_impl in step_registry.get_hooks('before', scenario.get_tags()):
-        hook_impl.run(scenario)
-    
-    for step in scenario.iter_steps():
-        
-        called = False
-        try:
-            runner.run_step(step)
-        except AssertionError as e:
-            handler.step_failed(step, e)
-            called = True
-        except fresher.stepregistry.UndefinedStepImpl as e:
-            handler.step_undefined(step, e)
-            called = True
-        except fresher.stepregistry.AmbiguousStepImpl as e:
-            handler.step_ambiguous(step, e)
-            called = True
-        except Exception as e:
-            handler.step_exception(step, e)
-            called = True
-        
-    for hook_impl in step_registry.get_hooks('after', scenario.get_tags()):
-        hook_impl.run(scenario)
-
-def run_feature(step_registry, feature, handler):
-    fresher.context.ftc.clear()
-    for scenario in feature.iter_scenarios():
-        run_scenario(step_registry, scenario, handler)
-    if fresher.context.glc.itp_list is None:
-        fresher.context.glc.itp_list = []
-    fresher.context.glc.itp_list.append(fresher.context.ftc.itp)
-
-def run_features(step_registry, features, handler):
-    for feature in features:
-        run_feature(step_registry, feature, handler)
-
-def load_step_definitions(paths):
+def new_load_step_definitions(paths):
     loader = fresher.stepregistry.StepImplLoader()
     sr = fresher.stepregistry.StepImplRegistry(fresher.core.TagMatcher)
     for path in paths:
         loader.load_steps_impl(sr, path, path)
     return sr
 
-def load_features(paths, language):
-    result = []
-    for path in paths:
-        for (dirpath, dirnames, filenames) in os.walk(path):
-            for feature_file in filenames:
-                if feature_file.endswith(".feature"):
-                    feature_file = os.path.join(dirpath, feature_file)
-                    result.append(fresher.core.load_feature(feature_file, language))
-    return result
+def new_run_features(step_registry, features, handler):
+    if fresher.context.glc.itp_list is None:
+        fresher.context.glc.itp_list = []
+    for feature in features:
+        fresher.cuke.run_feature(step_registry, feature, handler)
+        fresher.context.glc.itp_list.append(fresher.context.ftc.itp)
 
-def get_itp_from_feature(paths):
+def get_itp_from_feature(language,paths):
     """
+        the load_steps_impl function at load_step_definitions in fresher.cuke only has 2 arguments,has problem:
+
+        >>> import fresher.cuke
+        >>> import fresher.core
+        >>> import fresher.context
+        >>> import fresher.stepregistry
+        >>> paths = sys.argv[1:] or ["features"]
+        >>> fresher.context.glc.clear() 
+        >>> language = fresher.core.load_language('en')
+        >>> registry = fresher.cuke.load_step_definitions(paths)
+        Traceback (most recent call last):
+        ...
+        TypeError: load_steps_impl() missing 1 required positional argument: 'path'
+
         >>> import checkmate.parser.feature_visitor
-        >>> len(checkmate.parser.feature_visitor.get_itp_from_feature(['/home/followcat/Projects/checkmate/sample_app/itp']))
+        >>> len(checkmate.parser.feature_visitor.get_itp_from_feature('en',['/home/followcat/Projects/checkmate/sample_app/itp']))
         4
     """
     #logging.basicConfig(level=logging.DEBUG)
     fresher.context.glc.clear() 
-    language = fresher.core.load_language('en')
-    registry = load_step_definitions(paths)
-    features = load_features(paths, language)
-    handler = FreshenHandlerProxy([fresher.handlers.ConsoleHandler()])
-    run_features(registry, features, handler)
+    language_set = fresher.core.load_language(language)
+    registry = new_load_step_definitions(paths)
+    features = fresher.cuke.load_features(paths, language_set)
+    handler = fresher.cuke.FresherHandlerProxy([fresher.cuke.FresherHandler()])
+    new_run_features(registry, features, handler)
     return fresher.context.glc.itp_list
 
 def feature_new_produce(array_items, exchange_module, state_modules=[]):
