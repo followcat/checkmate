4525c1573af6c78d36a01273f20e927dfd20a8b0
lyx <laiyingxiao@qq.com>
2014-02-25 18:59:41 +0800

    add features and steps that support zh-CN
    --HG--
    branch : freshen

--- sample_app/itp/steps.py
+++ sample_app/itp/steps.py
@@ -39,3 +39,29 @@ def after(sc):
     if fresher.ftc.itp is None:
         fresher.ftc.itp = []
     fresher.ftc.itp.append(fresher.scc.exchange)
+    
+
+@fresher.Given("^组件的状态(?:(\w+).(\w+))的值是([\w\(\)]+)$")
+def set_initial(component_name, state, value):
+    _initial = {}
+    _initial['component_name'] = component_name
+    _initial['state'] = state
+    _initial['value'] = value
+    fresher.scc.exchange['Init'].append(_initial)
+
+@fresher.When("^组件(\w+)发出信号(\w+) ([\w\(\)]+)$")
+def set_incoming(component, exchange, action):
+    _incoming = {}
+    _incoming['component_name'] = component
+    _incoming['action'] = exchange
+    _incoming['value'] = action
+    fresher.scc.exchange['Action'].append(_incoming)
+
+@fresher.Then("^组件的状态(?:(\w+).(\w+))的值变成([\w\(\)]+)$")
+def set_final(component_name, state, value):
+    _final = {}
+    _final['component_name'] = component_name
+    _final['state'] = state
+    _final['value'] = value
+    fresher.scc.exchange['Final'].append(_final)
+
