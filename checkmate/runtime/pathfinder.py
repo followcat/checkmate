4bef3c820d39de7401595c88b5fd663e9bef62e4
QY Zhang <zqy2084@163.com>
2014-03-14 17:54:23 +0800

    add function for getting path to transiform to initial states
    --HG--
    branch : path_finder

--- checkmate/runtime/pathfinder.py
+++ checkmate/runtime/pathfinder.py
@@ -1,3 +1,4 @@
+import copy
 import checkmate.runtime.test_plan
 
 
@@ -38,3 +39,59 @@ def get_path_from_pathfinder(application_class, application, target):
 
     for proc in setup[2:]:
         yield proc
+
+def get_transition_from_pathfinder(application, initial, runs):
+    """
+        >>> import checkmate.runtime.pathfinder
+        >>> import checkmate.test_data
+        >>> import checkmate.sandbox
+        >>> a = checkmate.test_data.App()
+        >>> a.start()
+        >>> initial_states = a.components['C1'].state_machine.transitions[2].initial + a.components['C3'].state_machine.transitions[2].initial
+        >>> a.components['C3'].states[0].value
+        'False'
+        >>> runs = [[a.components['C1'].state_machine.transitions[0]], [a.components['C3'].state_machine.transitions[1]], [a.components['C1'].state_machine.transitions[2]]]
+        >>> box = checkmate.sandbox.Sandbox(type(a), initial_application=a)
+        >>> transitions = checkmate.runtime.pathfinder.get_transition_from_pathfinder(a, initial_states, runs)
+        >>> len(transitions)
+        2
+        >>> (transitions[0][0].incoming[0].factory().action, transitions[1][0].incoming[0].factory().action)
+        ('AC', 'RL')
+    """
+    box = checkmate.sandbox.Sandbox(type(application), initial_application=application)
+    path = []
+    for _run in runs:
+        if box(_run):
+            if compare_states(box.application, initial):
+                path.append(_run)
+            index = runs.index(_run)
+            if index < len(runs)-1:
+                new_runs = runs[:index] + runs[index+1:]
+                tmp_path = get_transition_from_pathfinder(box.application, initial, new_runs)
+                if tmp_path is not None and len(tmp_path) > 0:
+                    path += [_run] + tmp_path
+                    break
+            else:
+                break
+        else:
+            continue
+    if len(path) > 0:
+        return path
+
+def compare_states(application, target):
+    """"""
+    matching = 0
+    for _target in target:
+        for _component in list(application.components.values()):
+            try:
+                #Assume at most one state of component implements interface
+                _state = [_s for _s in _component.states if _target.interface.providedBy(_s)].pop(0)
+                if _state == _target.factory():
+                    matching += 1
+                    break
+                else:
+                    break
+            except IndexError:
+                continue
+    return matching == len(target)
+
