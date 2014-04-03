8e0224a43a280b1472a4be7d221f4960f388587f
Vincent Croisier <vcroisier.beijing@gmail.com>
2014-04-03 21:10:31 +0800

    Update feature generator to use Sandbox and 'Given' as initial state
    --HG--
    branch : storage

--- checkmate/runtime/test_plan.py
+++ checkmate/runtime/test_plan.py
@@ -1,6 +1,8 @@
 import checkmate._tree
+import checkmate.sandbox
 import checkmate.test_data
 import checkmate.service_registry
+import checkmate.runtime.procedure
 import checkmate.parser.feature_visitor
 
 
@@ -81,19 +83,60 @@ def TestProcedureInitialGenerator(application_class=checkmate.test_data.App, tra
             _o = a.components[_e.destination].process([_e])
             yield build_procedure_with_initial(components, [_e], _o, _transition.initial, _transition.final, transition_list), origin.name, _e.action, _e.destination
 
-def TestProcedureFeaturesGenerator(application_class=checkmate.test_data.App, transition_list=None):
-    a = application_class()
-    a.start()
-    components = list(a.components.keys())
+
+def build_procedure(sandbox):
+    proc = checkmate.runtime.procedure.Procedure()
+    sandbox.fill_procedure(proc)
+    return proc
+
+def TestProcedureFeaturesGenerator(application_class=checkmate.test_data.App):
+    """
+        >>> import checkmate.sandbox
+        >>> import checkmate.parser.feature_visitor
+        >>> import sample_app.application
+        >>> _application = sample_app.application.TestData()
+        >>> components = list(_application.components.keys())
+        >>> state_modules = []
+        >>> for name in components:
+        ...         state_modules.append(_application.components[name].state_module)
+        >>> transition_list = checkmate.parser.feature_visitor.get_transitions_from_features(_application.exchange_module, state_modules)
+        >>> transition_list[0].incoming[0].code
+        'PP'
+        >>> box = checkmate.sandbox.Sandbox(_application, [transition_list[0]])
+        >>> box.application.components['C1'].states[0].value == transition_list[0].initial[0].arguments[0][0]
+        True
+        >>> box.application.compare_states(transition_list[0].initial)
+        True
+        >>> box([transition_list[0]], foreign_transitions=True)
+        True
+        >>> len(box.initial)
+        3
+
+        >>> import checkmate.runtime._pyzmq
+        >>> import checkmate.runtime._runtime
+        >>> import checkmate.runtime.test_plan
+        >>> r = checkmate.runtime._runtime.Runtime(sample_app.application.TestData, checkmate.runtime._pyzmq.Communication, threaded=True)
+        >>> r.setup_environment(['C1'])
+        >>> r.start_test()
+        >>> c1 = checkmate.runtime.registry.global_registry.getUtility(checkmate.component.IComponent, 'C1')
+        >>> c2 = checkmate.runtime.registry.global_registry.getUtility(checkmate.component.IComponent, 'C2')
+        >>> c3 = checkmate.runtime.registry.global_registry.getUtility(checkmate.component.IComponent, 'C3')
+        >>> procedures = []
+        >>> for p in checkmate.runtime.test_plan.TestProcedureFeaturesGenerator(sample_app.application.TestData):
+        ...     procedures.append(p[0])
+        >>> proc = procedures[0]
+        >>> proc(system_under_test=['C1'])
+        >>> r.stop_test()
+    """
+    _application = application_class()
+    components = list(_application.components.keys())
     state_modules = []
     for name in components:
-        state_modules.append(a.components[name].state_module)
-    if transition_list is None:
-        transition_list = checkmate.parser.feature_visitor.get_transitions_from_features(a.exchange_module, state_modules)
+        state_modules.append(_application.components[name].state_module)
+    transition_list = checkmate.parser.feature_visitor.get_transitions_from_features(_application.exchange_module, state_modules)
+
     for _transition in transition_list:
-        _incoming = _transition.incoming[0].factory()
-        origin = get_origin_component(_incoming, list(a.components.values()))
-        for _e in checkmate.service_registry.global_registry.server_exchanges(_incoming, origin.name):
-            _o = a.components[_e.destination].process([_e])
-            yield build_procedure_with_initial(components, [_e], _o, _transition.initial, _transition.final, transition_list), origin.name, _e.action, _e.destination
+        box = checkmate.sandbox.Sandbox(_application, [_transition])
+        box([_transition], foreign_transitions=True)
+        yield build_procedure(box), box.exchanges.root.origin, box.exchanges.root.action, box.exchanges.root.destination
 
