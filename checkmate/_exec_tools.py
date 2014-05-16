da9143f7d00b6d480b9097f9552a800f0826ed28
followcat <followcat@163.com>
2014-05-16 15:35:52 +0800

    Add file checkmate/_exec_tools.py (hg add checkmate/_exec_tools.py)
    --HG--
    branch : _utils_exec

---
+++ checkmate/_exec_tools.py
@@ -0,0 +1,34 @@
+def is_method(signature):
+    """
+        >>> is_method('item')
+        False
+        >>> is_method('M0()')
+        True
+        >>> is_method('Q(R=NORM)')
+        True
+        >>> is_method('M0("AUTO")')
+        True
+    """
+    if len(signature.split('(')) == 1:
+        return False
+    return True
+
+
+def get_function_name(signature):
+    """
+        >>> get_function_name('Action(R:ActionRequest)')
+        'Action'
+    """
+    return signature.split('(')[0]
+
+
+def get_function_parameters_list(signature):
+    """
+        >>> get_function_parameters_list('ActionRequest(P:ActionPriority, A:Attribute)')
+        ['P:ActionPriority', 'A:Attribute']
+    """
+    found_label = signature.find('(')
+    if found_label == -1:
+        return ''
+    temp_list = [_p.strip() for _p in signature[signature.find('('):][1:-1].split(',')]
+    return temp_list
