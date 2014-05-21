4d832b15f1dbce61bdc73daa2b346acbab18f3a8
followcat <followcat@163.com>
2014-05-21 16:42:38 +0800

    get_function_parameters_list has better compatibility: can split and get from string has ',' like A(B(C, D), E)
    --HG--
    branch : _utils_exec

--- checkmate/_exec_tools.py
+++ checkmate/_exec_tools.py
@@ -24,11 +24,24 @@ def get_function_name(signature):
 
 def get_function_parameters_list(signature):
     """
-        >>> get_function_parameters_list('ActionRequest(P:ActionPriority, A:Attribute)')
-        ['P:ActionPriority', 'A:Attribute']
+        >>> get_function_parameters_list("A0('AT1')")
+        ['AT1']
+        >>> get_function_parameters_list("ActionMix(AR(P = AP('HIGH', 'NORM', 'LOW')), R = AR(P = AP('HIGH', 'NORM'), A = A()), K = None, Value = 5)")
+        ["AR(P = AP('HIGH', 'NORM', 'LOW'))", "R = AR(P = AP('HIGH', 'NORM'), A = A())", 'K = None', 'Value = 5']
     """
     found_label = signature.find('(')
     if found_label == -1:
         return ''
-    temp_list = [_p.strip(' \'"') for _p in signature[signature.find('('):][1:-1].split(',')]
+    temp_list = []
+    temp_str = ''
+    left_count = 0
+    for _s in signature[found_label:][1:-1].split(','):
+        left_count += _s.count('(')
+        left_count -= _s.count(')')
+        temp_str += _s
+        if left_count == 0:
+            temp_list.append(temp_str.strip(' \'"'))
+            temp_str = ''
+            continue
+        temp_str += ','
     return temp_list
