5a38ef3278b9f6ab1976339092bf17e41863e259
Vincent Croisier <vcroisier.beijing@gmail.com>
2014-06-04 18:25:16 +0800

    Document some 'feature' of Application.compare_states()

--- checkmate/application.py
+++ checkmate/application.py
@@ -126,7 +126,26 @@ class Application(object):
                 self.stubs.pop(self.stubs.index(name))
 
     def compare_states(self, target):
-        """"""
+        """Comparison between the states of the application's components and a target.
+
+        This comparison is  taking the length of the target into account.
+        If a matching state is twice in the target, the comparison will fail.
+        This should probably be fixed.
+            >>> import sample_app.application
+            >>> app = sample_app.application.TestData()
+            >>> app.start()
+            >>> c1 = app.components['C1']
+            >>> c1.states[0].value
+            'True'
+            >>> t = c1.state_machine.transitions[0]
+            >>> t.initial[0].arguments
+            (('True',), {})
+            >>> app.compare_states(t.initial)
+            True
+            >>> target = t.initial + t.initial
+            >>> app.compare_states(target)
+            False
+        """
         if len(target) == 0:
             return True
 
