693e69c73078d429e2a752d3baea540e9f18e2e4
followcat <followcat@163.com>
2014-10-27 11:00:54 +0800

    Add test_data_definition_file path string into sample_app.
    --HG--
    branch : yaml_data_separation

--- sample_app/application.py
+++ sample_app/application.py
@@ -7,6 +7,7 @@ class TestData(checkmate.application.Application, metaclass=checkmate.applicatio
     __test__ = False
 
     exchange_definition_file = 'sample_app/exchanges.yaml'
+    test_data_definition_file = 'sample_app/test_data.yaml'
 
     component_classes = {('C1',): ('Component_1', {}),
                          ('C2',): ('Component_2', {}),
