b3a8452a5a8a230a3e19fbf11bf8f5520531c918
followcat <followcat@163.com>
2014-11-07 15:04:03 +0800

    Add doctest in encoder decode.
    --HG--
    branch : yaml_data_separation

--- checkmate/runtime/encoder.py
+++ checkmate/runtime/encoder.py
@@ -7,6 +7,15 @@ def encode(exchange):
 
 
 def decode(message):
+    """
+    >>> import sample_app.application
+    >>> import checkmate.runtime.encoder
+    >>> ac = sample_app.exchanges.AC()
+    >>> encode_exchange = checkmate.runtime.encoder.encode(ac)
+    >>> decode_exchange = checkmate.runtime.encoder.decode(encode_exchange)
+    >>> ac == decode_exchange
+    True
+    """
     exchange_type, exchange_value = pickle.loads(message)
     exchange = exchange_type(exchange_value)
     return exchange
