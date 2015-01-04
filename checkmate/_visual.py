980e0d55f3242a4d9d30cc432b3be2f6b6eaefb2
followcat <followcat@163.com>
2015-01-04 16:56:52 +0800

    Modify the format in visual_run. Remove unused param command.
    --HG--
    branch : log_replay

--- checkmate/_visual.py
+++ checkmate/_visual.py
@@ -1,39 +1,32 @@
-def visual_states(dump_states, level=0, command=""):
+def visual_states(dump_states, level=0):
     return_str = ""
     tab_space = ' ' * 6 * level
     for _c, states in dump_states.items():
         if len(states) == 0:
             continue
-        return_str += "\n\
-{command}{space}{owner}:".format(command=command, space=tab_space, owner=_c)
+        return_str += "\n{space}{owner}:".format(space=tab_space, owner=_c)
         for state, values in states.items():
-            return_str += "\n\
-{command}{space}  {state}:".format(command=command, space=tab_space, state=state)
+            return_str += "\n{space}  {state}:".format(space=tab_space, state=state)
             for name, value in values.items():
-                return_str += "\n\
-{command}{space}    {name}: {value}".format(command=command, space=tab_space, name=name, value=value)
+                return_str += "\n{space}    {name}: {value}".format(space=tab_space, name=name, value=value)
     return return_str
 
 
-def visual_run_steps(run, level=0, command=""):
+def visual_run_steps(run, level=0):
     visual_dump = run.visual_dump_steps()
     tab_space = ' ' * 6 * level
-    final_states = visual_states(visual_dump['states'], level + 1, command)
+    final_states = visual_states(visual_dump['states'], level + 1)
     string = "\n\
-{command}{space}|\n\
-{command}{space}|     +-----------------------+\n\
-{command}{space}|     | {incoming}\n\
-{command}{space}|_____|\n\
-{command}{space}      | {outgoing}\n\
-{command}{space}      +-----------------------+{final}\
-".format(command=command, space=tab_space, incoming=visual_dump['incoming'], outgoing=visual_dump['outgoing'], final=final_states)
+{space}|\n\
+{space}|_____|-{incoming}\n\
+{space}      |-{outgoing}\n{final}".format(space=tab_space, incoming=visual_dump['incoming'], outgoing=visual_dump['outgoing'], final=final_states)
     for element in run.nodes:
-        string += visual_run_steps(element, level + 1, command)
+        string += visual_run_steps(element, level + 1)
     return string
 
 
-def visual_run(run, level=0, command=""):
-    return_str = visual_states(run.visual_dump_initial(), level, command)
-    return_str += visual_run_steps(run, level, command)
-    return_str += visual_states(run.visual_dump_final(), level, command)
+def visual_run(run, level=0):
+    return_str = visual_states(run.visual_dump_initial(), level)
+    return_str += visual_run_steps(run, level)
+    return_str += visual_states(run.visual_dump_final(), level)
     return return_str
