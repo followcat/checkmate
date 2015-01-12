ea28a4c3d25d6fc7528120380eee5160e94ca462
followcat <followcat@163.com>
2015-01-12 17:07:12 +0800

    Remove unused doctest in procedure.py.
    --HG--
    branch : fill_procedure_in_run

--- checkmate/runtime/procedure.py
+++ checkmate/runtime/procedure.py
@@ -10,51 +10,6 @@ import checkmate.runtime.interfaces
 
 
 def _compatible_skip_test(procedure, message):
-    """
-        >>> import checkmate._tree
-        >>> import checkmate.runtime._runtime
-        >>> import checkmate.runtime._pyzmq
-        >>> import checkmate.runtime.procedure
-        >>> import sample_app.application
-        >>> r = checkmate.runtime._runtime.Runtime(sample_app.application.TestData, checkmate.runtime._pyzmq.Communication, threaded=True)
-        >>> sut = ['C1']
-        >>> r.setup_environment(sut)
-        >>> r.start_test()
-        >>> a = sample_app.application.TestData()
-        >>> c2 = a.components['C2']
-        >>> a.start()
-        >>> proc = checkmate.runtime.procedure.Procedure()
-
-    If you expect to output an exchange using generic_incoming(), strange things can happen:
-    You select the transition that outputs an 'RL':
-        >>> transition = c2.state_machine.transitions[3]
-
-    You execute it:
-        >>> _incoming = c2.process(transition.generic_incoming(c2.states))[0]
-
-    But you get the wrong output (because it takes the first transition matching
-    the generic_incoming (here this is transition index 1)
-    *** this issue no longer exist after USER added***
-        >>> _incoming.value
-        'RL'
-
-    You better use, the direct simulate() function with expected output:
-        >>> _incoming = c2.simulate(transition)[0]
-        >>> _incoming.value
-        'RL'
-
-        >>> a.components[_incoming.destination[0]].process([_incoming]) #doctest: +ELLIPSIS
-        Traceback (most recent call last):
-        ...
-        checkmate.component.NoTransitionFound: ...
-        >>> _outgoing = []
-        >>> setattr(proc, 'exchanges', checkmate._tree.Tree(_incoming, [checkmate._tree.Tree(_output, []) for _output in _outgoing]))
-        >>> proc(r)
-        Traceback (most recent call last):
-        ...
-        unittest.case.SkipTest: Procedure components do not match SUT
-        >>> r.stop_test()
-    """
     if hasattr(procedure.result, 'addSkip'):
         if procedure.result is not None:
             procedure.result.startTest(procedure)
