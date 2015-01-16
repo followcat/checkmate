9322e8956d00ad93e63a62e5fe3cd8a71a26e361
Vincent Croisier <vcroisier.beijing@gmail.com>
2015-01-16 12:57:35 +0800

    Line length: checkmate pyzmq Communication
    --HG--
    branch : pep8

--- checkmate/runtime/_pyzmq.py
+++ checkmate/runtime/_pyzmq.py
@@ -18,26 +18,39 @@ class Connector(checkmate.runtime.communication.Connector):
         >>> c3 = a.components['C3']
         >>> connector = checkmate.runtime._pyzmq.Connector(c1, c)
         >>> connector.initialize()
-        >>> connector.socket_dealer_in.TYPE == zmq.DEALER, connector.socket_pub.Type == zmq.DEALER, connector.socket_sub == None
-        (True, True, True)
+        >>> connector.socket_dealer_in.TYPE == zmq.DEALER
+        True
+        >>> connector.socket_pub.Type == zmq.DEALER
+        True
+        >>> connector.socket_sub == None
+        True
         >>> connector.close()
 
         >>> connector = checkmate.runtime._pyzmq.Connector(c2, c)
         >>> connector.initialize()
-        >>> connector.socket_dealer_in.TYPE == zmq.DEALER, connector.socket_pub == None, connector.socket_sub.TYPE == zmq.SUB
-        (True, True, True)
+        >>> connector.socket_dealer_in.TYPE == zmq.DEALER
+        True
+        >>> connector.socket_pub == None
+        True
+        >>> connector.socket_sub.TYPE == zmq.SUB
+        True
         >>> connector.close()
 
         >>> connector = checkmate.runtime._pyzmq.Connector(c3, c)
         >>> connector.initialize()
-        >>> connector.socket_dealer_in.TYPE == zmq.DEALER, connector.socket_pub == None, connector.socket_sub.TYPE == zmq.SUB
-        (True, True, True)
+        >>> connector.socket_dealer_in.TYPE == zmq.DEALER
+        True
+        >>> connector.socket_pub == None
+        True
+        >>> connector.socket_sub.TYPE == zmq.SUB
+        True
         >>> connector.close()
 
         >>> c.close()
     """
     def __init__(self, component, communication=None, is_reading=True):
-        super(Connector, self).__init__(component, communication=communication, is_reading=is_reading)
+        super(Connector, self).__init__(component,
+            communication=communication, is_reading=is_reading)
         self._name = component.name
         self.is_reading = is_reading
         self.broadcast_map = component.broadcast_map
@@ -50,15 +63,18 @@ class Connector(checkmate.runtime.communication.Connector):
 
         self.zmq_context = zmq.Context.instance()
         self._routerport = self.communication.get_routerport()
-        self._broadcast_routerport = self.communication.get_broadcast_routerport()
+        self._broadcast_routerport = \
+            self.communication.get_broadcast_routerport()
         self._publishport = self.communication.get_publishport()
 
     def initialize(self):
         super(Connector, self).initialize()
         if self.is_publish:
-            self.socket_pub = self.open_router_socket(zmq.DEALER, self._broadcast_routerport)
+            self.socket_pub = \
+                self.open_router_socket(zmq.DEALER, self._broadcast_routerport)
         if self.broadcast_map:
-            self.socket_sub = self.open_router_socket(zmq.SUB, self._publishport)
+            self.socket_sub = \
+                self.open_router_socket(zmq.SUB, self._publishport)
             for _cname in self.broadcast_map.values():
                 self.socket_sub.setsockopt(zmq.SUBSCRIBE, _cname.encode())
         self.socket_dealer_in = self.zmq_context.socket(zmq.DEALER)
@@ -86,9 +102,12 @@ class Connector(checkmate.runtime.communication.Connector):
     def send(self, exchange):
         """"""
         if exchange.broadcast:
-            self.socket_pub.send_multipart([exchange.origin.encode(), self.communication.encoder.encode(exchange)])
+            self.socket_pub.send_multipart([exchange.origin.encode(),
+                self.communication.encoder.encode(exchange)])
         else:
-            self.socket_dealer_out.send_multipart([exchange.destination[0].encode(), self.communication.encoder.encode(exchange)])
+            self.socket_dealer_out.send_multipart(
+                [exchange.destination[0].encode(),
+                self.communication.encoder.encode(exchange)])
 
 
 class Communication(checkmate.runtime.communication.Communication):
@@ -106,11 +125,13 @@ class Communication(checkmate.runtime.communication.Communication):
         >>> r.start_test()
         >>> c2_stub = r.runtime_components['C2']
         >>> c1_stub = r.runtime_components['C1']
-        >>> simulated_transition = r.application.components['C2'].state_machine.transitions[0]
+        >>> c2 = r.application.components['C2']
+        >>> simulated_transition = c2.state_machine.transitions[0]
         >>> o = c2_stub.simulate(simulated_transition)
-        >>> c1_stub.context.state_machine.transitions[0].is_matching_incoming(o)
+        >>> t = c1_stub.context.state_machine.transitions[0]
+        >>> t.is_matching_incoming(o)
         True
-        >>> c1_stub.validate(c1_stub.context.state_machine.transitions[0])
+        >>> c1_stub.validate(t)
         True
         >>> time.sleep(1)
         >>> r.stop_test()
@@ -120,7 +141,8 @@ class Communication(checkmate.runtime.communication.Communication):
     def __init__(self, component=None):
         """"""
         super(Communication, self).__init__(component)
-        self.logger = logging.getLogger('checkmate.runtime._pyzmq.Communication')
+        self.logger = \
+            logging.getLogger('checkmate.runtime._pyzmq.Communication')
         self.logger.info("%s initialize" % self)
         self.encoder = checkmate.runtime.communication.Encoder()
         self.router = checkmate.runtime.communication.Router(self.encoder)
