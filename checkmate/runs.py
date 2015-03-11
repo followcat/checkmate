c8c49b4d64a47106ff39368430231c62bb3be1f3
jeff <ZhangQuanyun@willendare.com>
2015-03-11 18:21:54 +0800

    Find issue with finding runs by unknown-owner incoming origin transition
    --HG--
    branch : pathfinder_improvement

--- checkmate/runs.py
+++ checkmate/runs.py
@@ -214,7 +214,7 @@ def get_runs_from_transition(application, transition, itp_transition=False):
     return runs
 
 
-@checkmate.fix_issue('checkmate/issues/get_origin_transitions.rst')
+@checkmate.report_issue('checkmate/issues/get_origin_transitions.rst')
 def get_origin_transitions(application):
     origin_transitions = []
     for _component in application.components.values():
