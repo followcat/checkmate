24f6e8d8841544f8d0fcdaef80e10b2ac7ef5ddf
Sun Shaolei <sunshaolei@willendare.com>
2015-03-12 18:23:21 +0800

    move update_header to checkmate/tools and update string style
    --HG--
    branch : update_header

--- checkmate/tools/update_header/update_header.py
+++ checkmate/tools/update_header/update_header.py
@@ -5,38 +5,28 @@ import datetime
 
 notice_head = """\
 # This code is part of the checkmate project.
-# Copyright (C) 2013 The checkmate project
-contributors
+# Copyright (C) 2013 The checkmate project contributors
 # 
 """
 
 short_free_software = """\
-# This program is free software under the terms of the GNU GPL,
-either
-# version 3 of the License, or (at your option) any later
-version.\n\n
+# This program is free software under the terms of the GNU GPL, either
+# version 3 of the License, or (at your option) any later version.
 """
 
 long_free_software = """\
-# This program is free software: you can redistribute it and/or
-modify
-# it under the terms of the GNU General Public License as
-published by
-# the Free Software Foundation, either version 3 of the License,
-or
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# This program is distributed in the hope that it will be
-useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 # 
-# You should have received a copy of the GNU General Public
-License
-# along with this program. If not, see
-<http://www.gnu.org/licenses/>.
-\n
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 def main(path):
