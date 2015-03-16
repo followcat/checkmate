92ff4592cf9a33d71bba8e7421c03eabcc298799
jeff <ZhangQuanyun@willendare.com>
2015-03-16 16:50:34 +0800

    Add component instance_transitions to component definition
    --HG--
    branch : pep8

--- checkmate/application.py
+++ checkmate/application.py
@@ -101,16 +101,21 @@ class ApplicationMeta(type):
                 checkmate._module.get_module(namespace['__module__'],
                     class_name.lower(), alternative_package)
             instance_attributes = collections.defaultdict(dict)
+            instance_transitions = collections.defaultdict(dict)
             for _instance in class_definition['instances']:
                 if 'attributes' in _instance:
                     instance_attributes[_instance['name']] = \
                         _instance['attributes']
+                if 'transitions' in _instance:
+                    instance_transitions[_instance['name']] = \
+                        _instance['transitions']
             d = {'exchange_module': exchange_module,
                  'data_structure_module': data_structure_module,
                  'component_definition': class_definition['class'],
                  '__module__': component_module.__name__,
                  'communication_list': namespace['communication_list'].keys(),
-                 'instance_attributes': instance_attributes
+                 'instance_attributes': instance_attributes,
+                 'instance_transitions': instance_transitions
                 }
             d.update(class_dict)
             _class = checkmate.component.ComponentMeta(class_name,
