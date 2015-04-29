6e34165b9a1cfaeb715959cfb3b4c970936a5e21
Vincent Croisier <vcroisier.beijing@gmail.com>
2015-04-29 12:53:46 +0800

    Improve doctest by removing duplicate code
    --HG--
    branch : update_header

--- checkmate/tools/update_header.py
+++ checkmate/tools/update_header.py
@@ -45,11 +45,18 @@ def main():
                 elif filepath.endswith('.py'):
                     remove_header(filepath, notice_head + short_free_software)
             elif filepath.endswith('__init__.py'):
-                update_header(filepath, notice_head + long_free_software) 
+                with open(filepath, 'r') as f:
+                    content = f.read()
+                    content = update_header(content, notice_head +
+                                long_free_software) 
             elif filepath.endswith('.py'):
-                update_header(filepath, notice_head + short_free_software)
+                with open(filepath, 'r') as f:
+                    content = f.read()
+                    content = update_header(content, notice_head +
+                                short_free_software)
+                    write_content(filepath, content)
 
-def update_header(input_file, header):
+def update_header(content, header):
     """
     >>> import re
     >>> import datetime
@@ -61,44 +68,27 @@ def update_header(input_file, header):
     # Copyright (C) 2012 The checkmate project contributors
     #
     <BLANKLINE>
-    >>> m = re.search(r".*Copyright\D+(\d[0-9-, ]+\d)",head)
-    >>> m.group(1)
-    '2012'
-    >>> d = datetime.date.today()
-    >>> years = m.group(1)
-    >>> if not str(d.year) in years:
-    ...     if len(years) == 4:
-    ...         new_years = years + '-' + str(d.year)
-    ...     else:
-    ...         new_years = years[:4] + '-' + str(d.year)
-    ... 
-    >>> new_years
-    '2012-2015'
-    >>> print(head.replace(years, new_years))
+    >>> update = checkmate.tools.update_header.update_header(head, "")
+    >>> print(update) #doctest: +ELLIPSIS
     # This code is part of the checkmate project.
-    # Copyright (C) 2012-2015 The checkmate project contributors
-    #
-    <BLANKLINE>
+    # Copyright (C) 2012-2...
     """
-    with open(input_file, 'r') as f:
-        content = f.read()
-        if not content.startswith('#'):
-            f.seek(0, 0)
-            content = header + content
-            write_content(input_file, content)
-        else:
-            d = datetime.date.today()
-            match = re.search(r".*Copyright\D+(\d[0-9-, ]+\d)\D", content)
-            if match is not None:
-                years = match.group(1).strip()
-                if not str(d.year) in years:
-                    if len(years) == 4:
-                        new_years = years + '-' + str(d.year)
-                    else:
-                        new_years = years[:4] + '-' + str(d.year)
-                    content = content.replace(years,new_years)
-                    write_content(input_file, content)
-        f.close()
+    if not content.startswith('#'):
+        f.seek(0, 0)
+        content = header + content
+        write_content(input_file, content)
+    else:
+        d = datetime.date.today()
+        match = re.search(r".*Copyright\D+(\d[0-9-, ]+\d)\D", content)
+        if match is not None:
+            years = match.group(1).strip()
+            if not str(d.year) in years:
+                if len(years) == 4:
+                    new_years = years + '-' + str(d.year)
+                else:
+                    new_years = years[:4] + '-' + str(d.year)
+                content = content.replace(years,new_years)
+    return content
 
 def remove_header(input_file, header):
     content = ''
