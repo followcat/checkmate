58afa5ee8b13954bdb082bdbf4f7728db484722f
jeff <ZhangQuanyun@willendare.com>
2015-08-03 09:50:18 +0800

    Set current destination to a copy incoming exchange
    --HG--
    branch : component_engine

--- checkmate/sandbox.py
+++ checkmate/sandbox.py
@@ -157,10 +157,13 @@ class Sandbox(object):
                 _c = self.application.components[_d]
                 blocks = _c.get_blocks_by_input([_exchange])
                 _outgoings = _c.process([_exchange])
+                exchange = type(_exchange)()
+                exchange.carbon_copy(_exchange)
+                exchange.origin_destination(_exchange.origin, [_d])
                 states = _c.copy_states()
                 tmp_run = self.process(_outgoings,
                             checkmate.runs.Run(blocks[0], [],
-                                states=states, exchanges=[_exchange]))
+                                states=states, exchanges=[exchange]))
                 tree.add_node(tmp_run)
         return tree
 
