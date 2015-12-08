a59c84d87075f48b76677e47cf8baccf6be885a0
Vincent Croisier <vcroisier.beijing@gmail.com>
2015-12-08 17:33:02 +0800

    Add checkmate._scope for scope definition
    --HG--
    branch : feature_example

---
+++ checkmate/_scope.py
@@ -0,0 +1,19 @@
+import yaml
+
+
+class Scope(object):
+    """"""
+    def __init__(self, definition):
+        self.definition = yaml.load(definition)
+
+    @property
+    def description(self):
+        try:
+            return self.definition['description']
+        except ValueError:
+            return ""
+
+    @property
+    def backlog(self):
+        yield from self.definition.backlog
+
