2aab83dc04cb401411c3f6dfb5bc3ee1c63c21b6
Vincent Croisier <vcroisier.beijing@gmail.com>
2015-12-08 17:33:38 +0800

    Add run_feature to _scope.
    --HG--
    branch : feature_example

--- checkmate/_scope.py
+++ checkmate/_scope.py
@@ -1,4 +1,22 @@
+import os
 import yaml
+import doctest
+
+
+__all__ = ['check_backlog']
+
+
+def check_backlog(filename):
+    if os.path.isfile(filename):
+        _f = open(filename)
+    else:
+        _f = open(os.path.sep.join([os.getenv('CHECKMATE_HOME'), filename]))
+    definition = _f.read()
+    _f.close()
+    scope = Scope(definition)
+    runner = doctest.DocTestRunner()
+    for feature in scope.backlog:
+        scope.run_feature(feature, runner, filename)
 
 
 class Scope(object):
@@ -16,4 +34,9 @@ class Scope(object):
     @property
     def backlog(self):
         yield from self.definition['backlog']
+
+    def run_feature(self, feature, runner, filename):
+        test = feature['example']
+        runner.run(doctest.DocTestParser().get_doctest(test, locals(),
+                          filename, None, None))
 
