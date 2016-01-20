906ec183bb00c3c35260b46bfd51ad46aa7dce01
Vincent Croisier <vcroisier.beijing@gmail.com>
2016-01-20 10:02:57 +0800

    Update copyright 2016

--- checkmate/runtime/pathfinder.py
+++ checkmate/runtime/pathfinder.py
@@ -1,5 +1,5 @@
 # This code is part of the checkmate project.
-# Copyright (C) 2014-2015 The checkmate project contributors
+# Copyright (C) 2014-2016 The checkmate project contributors
 #
 # This program is free software under the terms of the GNU GPL, either
 # version 3 of the License, or (at your option) any later version.
