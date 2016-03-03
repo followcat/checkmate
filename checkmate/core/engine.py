604ba88d4e03427b7e1a0319bae155188d248a64
jeff <zqy2084@163.com>
2016-03-03 18:11:32 +0800

    Add base classes into checkmate/core/

---
+++ checkmate/core/engine.py
@@ -0,0 +1,13 @@
+class Engine(object):
+    """"""
+    def __init__(self):
+        """"""
+
+    def start(self):
+        """"""
+
+    def process(self):
+        """"""
+
+    def validate(self):
+        """"""
