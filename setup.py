b7e888952a6b1055ae0c30b95235c0462680f609
followcat <followcat@163.com>
2016-04-07 11:11:52 +0800

    Add setup.py and git ignore file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name="checkmate",
+    version="0.10",
+    description="Checkmate automatic tesing",
+    author="The checkmate Authors",
+    url="http://www.willendare.com",
+    license="GPLv3+",
+    package_dir = {'checkmate': 'checkmate'},
+    # packages = ['checkmate.core', 'checkmate.util'],
+    )
